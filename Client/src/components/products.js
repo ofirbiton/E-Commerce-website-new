f1a4c653c69aaaee717903cb534af221ca5fe173
Ofir Biton <143535334+ofirbiton@users.noreply.github.com>
2024-08-26 01:38:16 +0300

    errors messages added - checkout.js was changed and also checkout.css.

--- Client/src/components/products.js
+++ Client/src/components/products.js
@@ -18,7 +18,7 @@ const Products = ({ addToCart }) => {
         <div key={index} className="product">
           <img src={product.image} alt={product.title} />
           <h3>{product.title}</h3>
-          <span className="item-price">₪{product.price}</span>
+          <span className="item-price">ILS{product.price}</span>
           <button className="addToCart" onClick={() => addToCart(product)}>ADD TO BAG</button>
         </div>
       ))}
